60b5b3834c36493e498489b34040fbc8c031b589
Nwhis <17502190+Nwhis@users.noreply.github.com>
2024-07-10 10:36:02 -0500

    longer notifications

--- lua/autorun/client/cl_lagdetect.lua
+++ lua/autorun/client/cl_lagdetect.lua
@@ -6,7 +6,7 @@ net.Receive("lagdetect_notify",function()
     local islag = net.ReadBool()
     if not islag then
         --notification.AddLegacy("[LagDetect] Physics timescale restored!",0,3)
-        bs.Notify("[LagDetect] Physics timescale restored!",3,Color(50,255,0),true)
+        bs.Notify("[LagDetect] Physics timescale restored!",4,Color(50,255,0),true)
         MsgC(p,m,Color(50,255,0),"No lag detected, timescale returned to normal!\n")
         return
     end
@@ -17,7 +17,7 @@ net.Receive("lagdetect_notify",function()
     local t = math.Round(net.ReadFloat(),2)
 
     --notification.AddLegacy("[LagDetect] Physics timescale set to "..tostring(math.Round(speed,2)),0,6)
-    bs.Notify("[LagDetect] Physics timescale set to "..tostring(math.Round(speed,2)),5,Color(255,200,0),true)
+    bs.Notify("[LagDetect] Physics timescale set to "..tostring(math.Round(speed,2)),6,Color(255,200,0),true)
     local svrc = HSVToColor(math.Max(90 - t*0.8,0),0.8,1)
     MsgC(p,m,msgcolor,"Physics lag detected (last tick required ",
         svrc,tostring(t).."ms",
@@ -32,7 +32,7 @@ net.Receive("lagdetect_notify",function()
     end
 
     --notification.AddLegacy("[LagDetect] "..n.." has "..tostring(num).." intersecting props ("..tostring(percent).."%)",0,8)
-    bs.Notify("[LagDetect] "..n.." has "..tostring(num).." intersecting props ("..tostring(percent).."%)",8,Color(255,125,0),true)
+    bs.Notify("[LagDetect] "..n.." has "..tostring(num).." intersecting props ("..tostring(percent).."%)",10,Color(255,125,0),true)
     MsgC(p,m,c,n,
         msgcolor," [",s,"] has ",
         Color(255,60,40),num,
