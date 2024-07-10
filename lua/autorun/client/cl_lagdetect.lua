14039e14d5d923768d94aa970d18a1f0e71efbd1
Nwhis <17502190+Nwhis@users.noreply.github.com>
2024-07-09 21:42:47 -0500

    Initial commit

---
+++ lua/autorun/client/cl_lagdetect.lua
@@ -0,0 +1,33 @@
+local msgcolor = Color(255,255,255)
+local p,m = Color(255,100,25),"[LagDetect] "
+
+MsgC(p,m,Color(255,255,255),"Client Loaded!\n")
+net.Receive("lagdetect_notify",function()
+    local ply = net.ReadEntity()
+    if not IsValid(ply) then
+        notification.AddLegacy("[LagDetect] Physics timescale restored!",0,3)
+        MsgC(p,m,Color(50,255,0),"No lag detected, timescale returned to normal!\n")
+        return
+    end
+    local num = net.ReadUInt(10)
+    local percent = net.ReadUInt(7)
+    local speed = math.Round(net.ReadFloat(),2)
+    local t = math.Round(net.ReadFloat(),2)
+
+    notification.AddLegacy("[LagDetect] Physics timescale set to "..tostring(math.Round(speed,2)),0,6)
+    local svrc = HSVToColor(math.Max(90 - t*0.8,0),0.8,1)
+    MsgC(p,m,msgcolor,"Physics lag detected (last tick required ",
+        svrc,tostring(t).."ms",
+        msgcolor,")! Slowing physics to ",
+        Color(120,200,255),tostring(speed).."x\n")
+    
+    if not LocalPlayer():IsAdmin() then return end
+
+    notification.AddLegacy("[LagDetect] "..ply:GetName().." has "..tostring(num).." intersecting props ("..tostring(percent).."%)",0,8)
+    MsgC(p,m,team.GetColor(ply:Team()),ply:GetName(),
+        msgcolor," [",ply:SteamID(),"] has ",
+        Color(255,60,40),num,
+        msgcolor," possible intersecting ents (",
+        HSVToColor(90-percent*0.9,0.8,1),percent,"%",
+        msgcolor,")\n")
+end)
---
+++ lua/autorun/server/sv_lagdetect.lua
@@ -0,0 +1,112 @@
+local threshold_start = {150,60,30,15} -- maximum processing time before slowing down, sorted most>least severe; 15ms = ~1 tick
+local speeds = {0,0.03,0.3,0.75} -- corresponding timescales to use
+local function Cooldown(level) return 3+(#speeds-level)*1.5 end -- how long to stay at the slower speed before ramping back up
+
+local svrcolor = 90/#speeds
+
+local speed = 1
+local cooldown = 0
+local t_raw,t = 0,0
+local level = #speeds+1
+local msgcolor = Color(255,255,255)
+local p,m = Color(255,50,25),"[LagDetect] "
+local cv = GetConVar("phys_timescale")
+util.AddNetworkString("lagdetect_notify")
+
+MsgC(p,m,msgcolor,"Loaded!\n")
+game.ConsoleCommand("phys_timescale 1\n")
+
+local function Defuse(svr)
+    local newspeed = speeds[svr]
+    local svrc = HSVToColor(-svrcolor + svr*svrcolor,0.8,1)
+    MsgC(p,m,msgcolor,(speed == 1 and "Lagging" or "Still lagging!").." (last tick required ",
+        svrc,tostring(math.Round(t,2)).."ms",
+        msgcolor,")! Slowing down to ",
+        Color(120,200,255),tostring(newspeed).."\n")
+    game.ConsoleCommand("phys_timescale "..tostring(newspeed).."\n")
+    speed = newspeed
+    -- find intersecting props
+    local intersects = {}
+    for _,ent in ipairs(ents.FindByClass("prop_physics")) do
+        ent = ent:GetPhysicsObject()
+        if not ent:IsPenetrating() then continue end
+        table.insert(intersects,ent)
+    end
+    if #intersects == 0 then
+        MsgC(p,m,msgcolor,"No intersecting entities detected!\n")
+        return
+    end
+    -- find owner of the most props
+    local owners = {}
+    for _,ent in ipairs(intersects) do
+        ent = ent:GetEntity():CPPIGetOwner()
+        owners[ent] = (owners[ent] and owners[ent] + 1 or 1)
+    end
+    local most = table.GetWinningKey(owners)
+    local total = 0
+    for ply,num in pairs(owners) do
+        total = total + num
+    end
+    local percent = math.Round((owners[most]/total)*100,1)
+    MsgC(p,m,Color(255,60,40),#intersects,
+        msgcolor," possible intersecting ents, ",
+        team.GetColor(most:Team()),most:GetName(),
+        msgcolor," [",most:SteamID(),"] has the most (",
+        HSVToColor(90-percent*0.9,0.8,1),owners[most],", ",percent,"%",
+        msgcolor,")\n")
+    net.Start("lagdetect_notify")
+    net.WriteEntity(most)
+    net.WriteUInt(owners[most],10)
+    net.WriteUInt(math.Round(percent),7)
+    net.WriteFloat(speed)
+    net.WriteFloat(t)
+    net.Broadcast()
+end
+
+local function CooldownDone() -- begin ramping timescale back up
+    MsgC(p,m,Color(175,255,75),"Low lag detected, returning to normal speed...\n")
+    timer.Create("recover",0.5,0,function()
+        if speed == 1 then
+            timer.Remove("recover")
+            level = #speeds+1
+            MsgC(p,m,Color(50,255,0),"No lag detected, timescale returned to normal!\n")
+            net.Start("lagdetect_notify") net.Broadcast()
+            return
+        end
+        speed = math.Round(math.Min(speed*1.33 + 0.01,1),2)
+        level = level + 0.5
+        game.ConsoleCommand("phys_timescale "..tostring(speed).."\n")
+    end)
+end
+
+hook.Add("Think","lagdetector",function()
+    local mult = 0.5 + speed*0.5
+    t_raw = physenv.GetLastSimulationTime()*1000
+    t = math.Round((t_raw-0.001)/mult,2)
+    for k,v in ipairs(threshold_start) do
+        if t > v then
+            if level == k then -- if the current level is lower (more severe) or the same as this
+                if timer.Exists("cooldown") and timer.TimeLeft("cooldown") < 1.5 then -- if timer is about to expire
+                    local ts = math.Round(cv:GetFloat(),2)
+                    if ts != speed then
+                        MsgC(p,m,"[WARN] ",Color(255,225,75),"Physics timescale was overridden! Readjusting...\n")
+                        level = #speeds+1
+                        timer.Remove("cooldown")
+                        timer.Remove("recover")
+                        return
+                    end
+                    MsgC(p,m,msgcolor,"Still lagging! (",HSVToColor(-svrcolor + k*svrcolor,0.8,1),tostring(t).."ms",msgcolor,") Maintaining timescale...\n")
+                    timer.Adjust("cooldown",Cooldown(level))
+                    timer.Start("cooldown")  -- refresh the cooldown
+                end
+                return
+            end
+            if level < k then return end
+            level = k
+            Defuse(level)
+            timer.Create("cooldown",Cooldown(level),1,CooldownDone)
+            timer.Remove("recover")
+            return
+        end
+    end
+end)
